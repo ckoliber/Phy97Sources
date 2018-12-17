d3bfadb0f950c24e079b9c3c86c45a1be8ded1d0
koliber <koliberr136a1@gmail.com>
2018-12-17 12:41:00 +0330

    Session 2

---
+++ src/ir/koliber/Ex7_DotPlot.java
@@ -0,0 +1,27 @@
+//package ir.koliber;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        String name1 = sc.next();
+//        String name2 = sc.next();
+//
+//        for(int a = 0 ; a < name1.length() ; a++){
+//            for(int b = 0 ; b < name2.length() ; b++){
+//                if(name1.charAt(a) == name2.charAt(b)){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+//
+//}
--- src/ir/koliber/Main.java
+++ src/ir/koliber/Main.java
@@ -1,26 +1,12 @@
 package ir.koliber;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    static String reverse(String s){
-        String result = "";
-
-        for(int a = s.length() - 1 ; a >= 0 ; a--){
-            result = result + s.charAt(a);
-        }
-
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-
-        String rev = reverse(s);
-
-        System.out.println(rev);
 
     }
 
