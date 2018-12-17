124c688e0d14b1cb5917504ce42779e0f84418d1
koliber <koliberr136a1@gmail.com>
2018-12-17 13:20:18 +0330

    Session 2

---
+++ src/ir/koliber/Ex8_Random.java
@@ -0,0 +1,36 @@
+//package ir.koliber;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    static char get_random_character(){
+//        int rand = (int) (Math.random() * 52);
+//
+//        if(rand <= 25){
+//            return (char) (rand+65);
+//        }else{
+//            return (char) (rand+71);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        String random = "";
+//
+//        int length = sc.nextInt();
+//
+//
+//        for(int a = 0 ; a < length ; a++){
+//            char randomm_char = get_random_character();
+//
+//            random += randomm_char;
+//        }
+//
+//        System.out.println(random);
+//
+//    }
+//
+//}
